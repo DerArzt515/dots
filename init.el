37e74a21dd72237dcbf2462eab6a95f5e147833d
Matt Cavalier <matthewjcavalier@pm.me>
2021-11-02 08:01:11 -0500

    Added in general and dashboard to init.el

--- init.el
+++ init.el
@@ -67,6 +67,7 @@
 (menu-bar-mode -1)            ; Disable the menu bar
 
 (use-package evil
+  :diminish
   :init
 	    (setq evil-want-keybinding nil)
              :config
@@ -97,7 +98,7 @@
   (doom-modeline-mode 1))
 
 ;; Set up the visible bell
-(setq visible-bell t)
+;;(setq visible-bell t)
 
 (use-package ivy
              :diminish 
@@ -170,6 +171,82 @@
 
 (use-package magit
   )
+
+
+(use-package general
+  :ensure t
+  :config
+
+  (general-create-definer arzt/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+			  )
+
+(defun reload-conf-file ()
+  "This is to reload the main config file for emacs"
+  (interactive)
+  (load "~/.emacs.d/init.el"))
+
+;; This is stuff for enabling key bindings for major modes akin to
+;; what spacemacs and doom do
+;; ripped from: https://gist.github.com/progfolio/1c96a67fcec7584b31507ef664de36cc
+(general-create-definer global-leader
+  :keymaps 'override
+  :states '(emacs normal hybrid motion visual operator)
+  :prefix "SPC m"
+  "" '(:ignore t :which-key "leader"))
+
+(arzt/leader-keys
+ "t" '(:ignore t :which-key "toggles")
+ "tt" '(counsel-load-theme :which-key "choose theme")
+ "b" '(:ignore t :which-key "buffers")
+ "bb" '(counsel-switch-buffer :which-key "choose buffer")
+ "bp" '(previous-buffer :which-key "previous buffer")
+ "bn" '(next-buffer :which-key "next buffer")
+
+ ;; WINDOWS
+ "w" '(:ignore t :which-key "windows")
+ "ws" '(evil-window-split :which-key "split to above & below")
+ "w/" '(evil-window-vsplit :which-key "split to left and right")
+ "wj" '(evil-window-down :which-key "go down")
+ "wk" '(evil-window-up :which-key "go up")
+ "wh" '(evil-window-left :which-key "go left")
+ "wl" '(evil-window-right :which-key "go right")
+ "q" '(:ignore t :which-key "system")
+ "qr" '(reload-conf-file :which-key "reload main conf")
+ "qq" '(evil-quit-all :which-key "exit emacs")
+
+ "e" '(:ignore t :which-key "eval")
+ "eb" '(eval-buffer :which-key "buffer")
+
+ ;; FILES
+ "f" '(:ignore t :which-key "files")
+ "ff" '(counsel-find-file :which-key "find file")
+
+ ;; Search
+ "s" '(swiper :which-key "search current file")
+
+ ;; COMMAND
+ "SPC" '(counsel-M-x :which-key ":")
+ "!" '(shell-command :which-key "Shell CMD"))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-banner-logo-title "AWWWWW YIISSSSS"
+	dashboard-startup-banner "~/dots/derp.png"
+	dashboard-center-content t
+	dashbaord-items '((recents . 5)
+			  (bookmarks . 5)
+			  (projects . 5)
+			  (agenda . 5)
+			  (registers))
+	dashboard-set-heading-icons t
+	dashboard-set-file-icons t
+	dashboard-set-footer nil
+	dashbaord-set-navigator t))
 
 
 (custom-set-variables
